edab5ad3f673dac8a730380af5504b1cf497a663
seungjo <f1v3@kakao.com>
2024-06-14 16:00:13 +0900

    feat: login form

--- src/router/index.ts
+++ src/router/index.ts
@@ -3,33 +3,40 @@ import HomeView from '../views/HomeView.vue'
 import WriteView from '../views/WriteView.vue'
 import ReadView from '../views/ReadView.vue'
 import EditView from '../views/EditView.vue'
+import LoginView from '../views/LoginView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/write',
-      name: 'write',
-      component: WriteView
-    },
-    {
-      path: '/read/:postId',
-      name: 'read',
-      component: ReadView,
-      props: true
-    },
-    {
-      path: '/edit/:postId',
-      name: 'edit',
-      component: EditView,
-      props: true
-    }
-  ]
+    history: createWebHistory(import.meta.env.BASE_URL),
+    routes: [
+        {
+            path: '/',
+            name: 'home',
+            component: HomeView
+        },
+        {
+            path: '/write',
+            name: 'write',
+            component: WriteView
+        },
+        {
+            path: '/read/:postId',
+            name: 'read',
+            component: ReadView,
+            props: true
+        },
+        {
+            path: '/edit/:postId',
+            name: 'edit',
+            component: EditView,
+            props: true
+        },
+        {
+            path: '/login',
+            name: 'login',
+            component: LoginView,
+            props: true
+        }
+    ]
 })
 
 export default router
---
+++ src/views/LoginView.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { reactive } from 'vue'
+import Login from '@/entity/user/Login'
+
+const state = reactive({
+    login: new Login()
+})
+
+function doLogin() {
+    console.log('>> ', state.login)
+}
+</script>
+
+<template>
+    <el-row>
+        <el-col :span="10" :offset="7">
+            <el-form label-position="top">
+                <el-form-item label="e-mail">
+                    <el-input v-model="state.login.email"></el-input>
+                </el-form-item>
+
+                <el-form-item label="password">
+                    <el-input type="password" v-model="state.login.password"></el-input>
+                </el-form-item>
+
+                <el-form-item>
+                    <el-button type="primary" style="width: 100%" @click="doLogin()">
+                        로그인
+                    </el-button>
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
+</template>
+
+<style scoped lang="scss"></style>
