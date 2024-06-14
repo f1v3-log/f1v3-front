642c662690e4ac1577c15c3ae6785472bfa88236
seungjo <f1v3@kakao.com>
2024-06-14 16:22:21 +0900

    feat: login 성공 및 실패 처리

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,17 +1,26 @@
 <script setup lang="ts">
 import { reactive } from 'vue'
 import Login from '@/entity/user/Login'
-import axios from 'axios'
-import router from '@/router'
+import axios, { type AxiosError } from 'axios'
+import { ElMessage } from 'element-plus'
+import { useRouter } from 'vue-router'
 
 const state = reactive({
     login: new Login()
 })
 
+const router = useRouter()
+
 function doLogin() {
-    axios.post('/f1v3-api/auth/login', state.login).then(() => {
-        router.replace({ name: 'home' })
-    })
+    axios
+        .post('/f1v3-api/auth/login', state.login)
+        .then(() => {
+            ElMessage({ type: 'success', message: '환영합니다.' })
+            router.replace('/')
+        })
+        .catch((e: AxiosError) => {
+            ElMessage({ type: 'error', message: e.response?.data.message })
+        })
 }
 </script>
 
