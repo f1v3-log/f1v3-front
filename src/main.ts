76eea9d03e27c8c2f83fe6779941423012a39e62
seungjo <f1v3@kakao.com>
2024-06-18 17:33:12 +0900

    feat: tsyringe 적용

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,17 @@
-
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
 
-import "normalize.css"
+import 'normalize.css'
 
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 
-import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import 'reflect-metadata'
 
 const app = createApp(App)
 
