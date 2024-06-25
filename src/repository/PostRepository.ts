fc0805fd85a5d02555f9dbc6432f34e6772d4e9a
seungjo <f1v3@kakao.com>
2024-06-25 14:53:15 +0900

    feat: 게시글 작성 API 연결

---
+++ src/repository/PostRepository.ts
@@ -0,0 +1,15 @@
+import HttpRepository from '@/repository/HttpRepository'
+import { inject, singleton } from 'tsyringe'
+import type PostWrite from '@/entity/post/PostWrite'
+
+@singleton()
+export default class PostRepository {
+    constructor(@inject(HttpRepository) private readonly httpRepository: HttpRepository) {}
+
+    public write(request: PostWrite) {
+        return this.httpRepository.post({
+            path: '/f1v3-api/posts',
+            body: request
+        })
+    }
+}
--- src/views/WriteView.vue
+++ src/views/WriteView.vue
@@ -1,41 +1,50 @@
 <script setup lang="ts">
-
-import { ref } from 'vue'
-import axios from 'axios'
+import { reactive } from 'vue'
 import { useRouter } from 'vue-router'
+import PostWrite from '@/entity/post/PostWrite'
+import { container } from 'tsyringe'
+import PostRepository from '@/repository/PostRepository'
+import { ElMessage } from 'element-plus'
+import type HttpError from '@/http/HttpError'
 
-const title = ref('')
-const content = ref('')
+const state = reactive({
+    postWrite: new PostWrite()
+})
+
+const POST_REPOSITORY = container.resolve(PostRepository)
 const router = useRouter()
 
-const write = function() {
-  axios.post(
-    '/f1v3-api/posts',
-    {
-      title: title.value,
-      content: content.value
-    })
-    .then(() => {
-      router.replace({ name: 'home' })
-    })
+function write() {
+    // POST_REPOSITORY.write(state.postWrite)
+    POST_REPOSITORY.write(state.postWrite)
+        .then(() => {
+            ElMessage({ type: 'success', message: '글 등록이 완료되었습니다.' })
+            router.replace('/')
+        })
+        .catch((e: HttpError) => {
+            ElMessage({ type: 'error', message: e.getMeesage() })
+        })
 }
-
 </script>
 
 <template>
-  <div>
-    <el-input v-model="title" placeholder="멋진 제목을 입력해보세요! 🚀" />
-  </div>
-
-  <div class="mt-2">
-    <el-input v-model="content" type="textarea" rows="15"/>
-  </div>
-
-  <div class="mt-2">
-    <div class="d-flex justify-content-end">
-      <el-button type="primary" round @click="write()">글 작성 완료</el-button>
+    <div>
+        <el-input
+            v-model="state.postWrite.title"
+            size="large"
+            placeholder="멋진 제목을 입력해보세요! 🚀"
+        />
     </div>
-  </div>
+
+    <div class="mt-2">
+        <el-input v-model="state.postWrite.content" type="textarea" rows="15" />
+    </div>
+
+    <div class="mt-2">
+        <div class="d-flex justify-content-end w-100">
+            <el-button type="primary" style="width: 100%" @click="write()">글 작성 완료</el-button>
+        </div>
+    </div>
 </template>
 
 <style></style>
