6777c9401e3b0b9a564e17d71d844c02e82dbe28
seungjo <f1v3@kakao.com>
2024-07-16 00:01:32 +0900

    feat: 로그인 사용자 정보 조회

--- src/repository/UserRepository.ts
+++ src/repository/UserRepository.ts
@@ -1,15 +1,24 @@
 import HttpRepository from '@/repository/HttpRepository'
 import type Login from '@/entity/user/Login'
 import { inject, singleton } from 'tsyringe'
+import UserProfile from '@/entity/user/UserProfile'
 
 @singleton()
 export default class UserRepository {
-    constructor(@inject(HttpRepository) private readonly httpRepository: HttpRepository) {}
+    constructor(@inject(HttpRepository) private readonly httpRepository: HttpRepository) {
+    }
 
     public login(request: Login) {
         return this.httpRepository.post({
             path: '/f1v3-api/auth/login',
             body: request
         })
+    }
+
+    public getProfile() {
+        return this.httpRepository.get({
+            path: '/f1v3-api/users/me'
+        }, UserProfile)
+
     }
 }
